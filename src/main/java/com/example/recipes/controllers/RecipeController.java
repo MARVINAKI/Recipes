4cfd3bde9ff1bfa653f217cedd551a2a8baf1102
MARVINAKI <kosakbig@gmial.com>
2023-01-21 13:50:08 +0500

    step_2

--- src/main/java/com/example/recipes/controllers/RecipeController.java
+++ src/main/java/com/example/recipes/controllers/RecipeController.java
@@ -4,12 +4,15 @@ import com.example.recipes.model.Ingredient;
 import com.example.recipes.model.Recipe;
 import com.example.recipes.services.IngredientService;
 import com.example.recipes.services.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
+@Tag(name = "Рецепты", description = "Создание и редактирование рецептов")
 @RequestMapping("/recipes")
 public class RecipeController {
     private final RecipeService recService;
@@ -22,6 +25,9 @@ public class RecipeController {
     }
 
     @PostMapping
+    @Operation(summary = "Создание нового рецепта",
+            description = "Создается новый рецепт и добавляется в базу данных рецптов, " +
+                    "также ингредиенты рецепта создаются автоматически и добавляются в базу данных ингредиентов")
     public Recipe addRecipe(@RequestBody Recipe recipe) {
         recService.addRecipe(recipe);
         for (Ingredient ingredient : recipe.getIngredients()) {
@@ -31,22 +37,31 @@ public class RecipeController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Поиск и вывод рецепта",
+            description = "Ищет, интересующий Вас, рецепт по id и выводит его")
     public ResponseEntity<Recipe> getRecipe(@PathVariable int id) {
         Recipe recipe = recService.getRecipe(id);
         return recipe == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(recipe);
     }
 
     @GetMapping("/find")
+    @Operation(summary = "Поиск рецепта(ов) по ингредиентам",
+            description = "По одному или более ингредиентам находит и выводит рецепт(ы) с содержанием данных ингредиентов")
     public ResponseEntity<Collection<Recipe>> findRecByIng(@RequestParam int... id) {
         return ResponseEntity.ok(recService.findRecByIng(id));
     }
 
     @GetMapping("/print")
+    @Operation(summary = "Поиск рецептов",
+            description = "Поиск и вывод полного списка рецептов")
     public Collection<Recipe> getAllRecipes() {
         return recService.getAllRecipes();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Изменение рецепта",
+            description = "Возможность изменить частично или полностью рецепт " +
+                    "(выбор объекта по id, обязательно создаем полностью объект для замены)")
     public ResponseEntity<Recipe> editRecipe(@PathVariable int id,
                                              @RequestBody Recipe newRecipe) {
         Recipe recipe = recService.editRecipe(id, newRecipe);
@@ -54,6 +69,8 @@ public class RecipeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление рецепта",
+            description = "Полное удаление рецепта из базы (выбор объекта по id). Удаление ингредиентов рецепта из базы не производится")
     public ResponseEntity<Void> deleteRecipe(@PathVariable int id) {
         return recService.deleteRecipe(id) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
